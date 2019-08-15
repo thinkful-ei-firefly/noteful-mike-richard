fe51975bd7378235f6f5cbab224f959b6e18b565
rjgates3 <gates.j.richard@gmail.com>
2019-08-15 11:13:30 -0600

    added an error boundry

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import App from './App';
 import history from './history';
+import ErrorBoundry from './ErrorBoundry';
 
 import './index.css';
 
 ReactDOM.render(
   <Router history={history}>
-    <App />
+    <ErrorBoundry>
+      <App />
+    </ErrorBoundry>
   </Router>,
   document.getElementById('root')
 );
